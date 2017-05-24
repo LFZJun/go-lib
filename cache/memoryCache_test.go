a9fb8e557579e67fca13a2b827fe36716634e6cc
LionJun <592368998@qq.com>
2017-05-24 20:53:14 +0800

    shared

--- cache/memoryCache_test.go
+++ cache/memoryCache_test.go
@@ -1,18 +1,64 @@
 package cache
 
 import (
-	"fmt"
+	"log"
 	"testing"
 	"time"
+
+	"github.com/allegro/bigcache"
+	"github.com/cocotyty/cache"
 )
 
-func TestCache(t *testing.T) {
-	m := NewCache()
-	m.Set("name", "ljun", time.Millisecond*500)
-	m.Set("name", "ljun", -1)
-	fmt.Println(m.Get("name"))
-	time.Sleep(time.Second * 1)
-	m.Set("name", "ljun", time.Millisecond*500)
-	time.Sleep(time.Second * 1)
-	fmt.Println(m.Get("name"))
+//func BenchmarkCache_Set(b *testing.B) {
+//	m := NewCache()
+//
+//	b.RunParallel(func(pb *testing.PB) {
+//		for pb.Next() {
+//			m.Set(time.Now().String(), time.Now().String(), time.Second)
+//		}
+//	})
+//}
+
+func BenchmarkCMap_Set(b *testing.B) {
+	cmap := cache.NewCMap(32)
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			cmap.Set(time.Now().String(), &cache.CacheValue{Exp: int64(time.Second), Value: time.Now().String()})
+		}
+	})
+}
+
+func BenchmarkBigCache_Set(b *testing.B) {
+	config := bigcache.Config{
+		// number of shards (must be a power of 2)
+		Shards: 32,
+		// time after which entry can be evicted
+		LifeWindow: 10 * time.Minute,
+		// rps * lifeWindow, used only in initial memory allocation
+		MaxEntriesInWindow: 1000 * 10 * 60,
+		// max entry size in bytes, used only in initial memory allocation
+		MaxEntrySize: 500,
+		// prints information about additional memory allocation
+		Verbose: true,
+		// cache will not allocate more memory than this limit, value in MB
+		// if value is reached then the oldest entries can be overridden for the new ones
+		// 0 value means no size limit
+		HardMaxCacheSize: 8192,
+		// callback fired when the oldest entry is removed because of its
+		// expiration time or no space left for the new entry. Default value is nil which
+		// means no callback and it prevents from unwrapping the oldest entry.
+		OnRemove: nil,
+	}
+
+	c, initErr := bigcache.NewBigCache(config)
+	if initErr != nil {
+		log.Fatal(initErr)
+	}
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			c.Set(time.Now().String(), []byte(time.Now().String()))
+		}
+	})
 }
