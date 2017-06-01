f3ae5b7f68f80456fabbcd42d2c3c04b6d0c07c1
LionJun <592368998@qq.com>
2017-06-01 11:53:26 +0800

    refacotr: cache

--- cache/memoryCache.go
+++ cache/memoryCache.go
@@ -1,73 +1,120 @@
 package cache
 
 import (
+	"context"
 	"sync"
 	"time"
+
+	"github.com/LFZJun/go-lib/cache/hashl"
 )
 
-type Value struct {
-	V      interface{}
-	expire time.Duration
-	done   chan struct{}
-}
+type (
+	Store struct {
+		Value    interface{}
+		Timeout  time.Duration
+		Deadline time.Time
 
-func (v *Value) CancelDel() {
-	if v.expire > 0 {
-		v.done <- struct{}{}
+		done   <-chan struct{}
+		cancel context.CancelFunc
 	}
-}
-
-type Maps struct {
-	Map   map[string]*Value
-	Mutex sync.RWMutex
-}
-
-func (m *Maps) tick(key string, value *Value) {
-	select {
-	case <-time.After(value.expire):
-		m.Del(key)
-	case <-value.done:
+	MapMutex struct {
+		Map   map[string]*Store
+		Mutex sync.RWMutex
 	}
-}
-
-func (m *Maps) Set(key string, value interface{}, expire time.Duration) {
-	var v *Value
-	switch {
-	case expire >= 0:
-		v = &Value{value, expire, make(chan struct{})}
-		defer func() { go m.tick(key, v) }()
-	default:
-		v = &Value{value, expire, nil}
+	TTL struct {
+		m *MapMutex
 	}
+	TTLCache struct {
+		size uint32
+		ttls []*TTL
+	}
+)
+
+func (m *MapMutex) Set(key string, store *Store) {
 	m.Mutex.Lock()
-	if vv, ok := m.Map[key]; ok {
-		vv.CancelDel()
-	}
-	m.Map[key] = v
+	m.Map[key] = store
 	m.Mutex.Unlock()
 }
 
-func (m *Maps) Get(key string) interface{} {
+func (m *MapMutex) Get(key string) *Store {
 	m.Mutex.RLock()
 	defer m.Mutex.RUnlock()
-	if v, ok := m.Map[key]; ok {
-		return v.V
-	}
-	return nil
+	return m.Map[key]
 }
 
-func (m *Maps) Del(key string) {
+func (m *MapMutex) Del(key string) {
 	m.Mutex.Lock()
 	delete(m.Map, key)
 	m.Mutex.Unlock()
 }
 
-type Cache interface {
-	Set(key string, value interface{}, expire time.Duration)
-	Get(key string) interface{}
-	Del(key string)
+func (t *TTL) tick(key string, s *Store) {
+	select {
+	case <-time.After(s.Timeout):
+		t.m.Del(key)
+	case <-s.done:
+	}
 }
 
-func NewCache() Cache {
-	return &Maps{Map: make(map[string]*Value)}
+func (t *TTL) Set(key string, s *Store) {
+	switch {
+	case s.Timeout >= 0:
+		ctx, cancel := context.WithCancel(context.Background())
+		s.done = ctx.Done()
+		s.cancel = cancel
+		go t.tick(key, s) // 随着 coroutine的数量上升，性能会急剧下降，所以说赶紧换个定时器吧
+	default:
+	}
+	if vv := t.m.Get(key); vv != nil && vv.Timeout >= 0 { // 会有一次读的性能损耗 采用这种定时策略
+		vv.cancel()
+	}
+	t.m.Set(key, s)
+}
+
+func (t *TTL) SetDeadline(key string, value interface{}, deadline time.Time) {
+	t.Set(key, &Store{Value: value, Timeout: time.Until(deadline), Deadline: deadline})
+}
+
+func (t *TTL) SetTimeout(key string, value interface{}, timeout time.Duration) {
+	t.Set(key, &Store{Value: value, Timeout: timeout, Deadline: time.Now().Add(timeout)})
+}
+
+func (t *TTL) Get(key string) *Store {
+	return t.m.Get(key)
+}
+
+func (tc *TTLCache) Get(key string) *Store {
+	index := hashl.HashIndex32(key, tc.size)
+	return tc.ttls[index].Get(key)
+}
+
+func (tc *TTLCache) Set(key string, s *Store) {
+	index := hashl.HashIndex32(key, tc.size)
+	tc.ttls[index].Set(key, s)
+}
+
+func (tc *TTLCache) SetDeadline(key string, value interface{}, deadline time.Time) {
+	index := hashl.HashIndex32(key, tc.size)
+	tc.ttls[index].SetDeadline(key, value, deadline)
+}
+
+func (tc *TTLCache) SetTimeout(key string, value interface{}, timeout time.Duration) {
+	index := hashl.HashIndex32(key, tc.size)
+	tc.ttls[index].SetTimeout(key, value, timeout)
+}
+
+func NewMapMutex() *MapMutex {
+	return &MapMutex{Map: make(map[string]*Store)}
+}
+
+func NewTTL() *TTL {
+	return &TTL{NewMapMutex()}
+}
+
+func NewTTLCache(size uint32) *TTLCache {
+	t := make([]*TTL, 0, size)
+	for i := 0; i < int(size); i++ {
+		t = append(t, NewTTL())
+	}
+	return &TTLCache{size: size, ttls: t}
 }
--- cache/memoryCache_test.go
+++ cache/memoryCache_test.go
@@ -5,12 +5,48 @@ import (
 	"time"
 )
 
-func BenchmarkCache_Get(b *testing.B) {
-	m := NewCache()
+func BenchmarkTTLCache_Get(b *testing.B) {
+	c := NewTTLCache(1 << 8)
 
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
-			m.Get(time.Now().String())
+			c.Get(time.Now().String())
+		}
+	})
+}
+
+func BenchmarkTTLCache_SetDeadline(b *testing.B) {
+	c := NewTTLCache(1 << 8)
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			now := time.Now().String()
+			c.SetDeadline(now, now, time.Now().Add(time.Second*10))
+		}
+	})
+}
+
+func BenchmarkTTL_SetDeadline(b *testing.B) {
+	t := NewTTL()
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			now := time.Now().String()
+			t.SetDeadline(now, now, time.Now().Add(time.Second*10))
+		}
+	})
+}
+
+func BenchmarkTTLCache_Set(b *testing.B) {
+	c := NewTTLCache(1 << 8)
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			now := time.Now().String()
+			c.Set(now, &Store{
+				Timeout: time.Second * 10,
+				Value:   time.Now().Add(time.Second * 10),
+			})
 		}
 	})
 }
