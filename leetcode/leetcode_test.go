b3bb84bedcbe14c2996fd12826e119c9ca845e48
LionJun <ljun20160606@gmail.com>
2017-07-10 17:57:55 +0800

    leetcode 1,2

---
+++ leetcode/leetcode_test.go
@@ -0,0 +1,21 @@
+package leetcode
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test1(t *testing.T) {
+	fmt.Println(twoSum([]int{1, 3, 2}, 1))
+}
+
+func Test2(t *testing.T) {
+	result := addTwoNumbers(
+		&listNode{5, nil},
+		&listNode{5, nil},
+	)
+	for result != nil {
+		fmt.Println(result.Val)
+		result = result.Next
+	}
+}
